21a3f78c20530a0818ce61b89bf7b309382c52ba
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2025-01-08 23:16:23 -0500

    Adding the different elements on the anime page

--- pages/anime/[id]/index.vue
+++ pages/anime/[id]/index.vue
@@ -1,5 +1,24 @@
 <template>
-    <h1>MyAnime</h1>
+    <div v-if="anime">
+        <img :src="anime.images.jpg.image_url" alt="poster">
+        <h3>
+            <span v-for="title in anime.titles" :key="title">{{ title.title }} | </span>
+        </h3>
+
+        <div>
+            <h4>Studio: </h4>
+            <span v-for="studio in anime.studios" :key="studio"> {{ studio.name }} |</span>
+        </div>
+        <div>
+            <h4>Genres: </h4>
+            <span v-for="genre in anime.genres" :key="genre"> {{ genre.name }} | </span>
+        </div>
+        <NuxtLink :href="anime.url">See on My Anime List</NuxtLink>
+        <NuxtLink :href="anime.trailer.url">See the trailer</NuxtLink>
+        <span>Status: {{ anime.status }}</span>
+        <span>Score: {{ anime.score }}</span>
+        <p>{{ anime.synopsis }}</p>
+    </div>
 </template>
 
 <script lang="ts" setup>
@@ -13,6 +32,8 @@ const anime = ref(null);
 
 watchEffect(() => {
     anime.value = fetched?.value?.data || null;
+
+    console.log(anime.value);
 })
 
 </script>
