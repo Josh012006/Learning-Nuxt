57d51a020717458edce4e54d7189aef04c07f851
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2025-01-08 23:35:34 -0500

    Finished styling the anime page

--- pages/anime/[id]/index.vue
+++ pages/anime/[id]/index.vue
@@ -1,23 +1,38 @@
 <template>
     <div v-if="anime">
-        <img :src="anime.images.jpg.image_url" alt="poster">
-        <h3>
-            <span v-for="title in anime.titles" :key="title">{{ title.title }} | </span>
-        </h3>
-
-        <div>
-            <h4>Studio: </h4>
-            <span v-for="studio in anime.studios" :key="studio"> {{ studio.name }} |</span>
+        <div class="heading">
+            <div class="div-img">
+                <img :src="anime.images.jpg.image_url" alt="poster">
+            </div>
+            <div>
+                <h3 class="titles">
+                    <span v-for="title in anime.titles" :key="title">{{ title.title }} | </span>
+                </h3>
+                <div class="production">
+                    <div>
+                        <h4>Studios: </h4>
+                        <span v-for="studio in anime.studios" :key="studio"> {{ studio.name }} |</span>
+                    </div>
+                    <div>
+                        <h4>Genres: </h4>
+                        <span v-for="genre in anime.genres" :key="genre"> {{ genre.name }} | </span>
+                    </div>
+                    <div>
+                        <h4>Themes: </h4>
+                        <span v-for="theme in anime.themes" :key="theme"> {{ theme.name }} | </span>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div>
-            <h4>Genres: </h4>
-            <span v-for="genre in anime.genres" :key="genre"> {{ genre.name }} | </span>
+        <div class="anime-info">
+            <span><b>Status:</b> {{ anime.status }}</span>
+            <span><b>Score:</b> {{ anime.score }}</span>
+            <p><b>Synopsis:</b> {{ anime.synopsis }}</p>
         </div>
-        <NuxtLink :href="anime.url">See on My Anime List</NuxtLink>
-        <NuxtLink :href="anime.trailer.url">See the trailer</NuxtLink>
-        <span>Status: {{ anime.status }}</span>
-        <span>Score: {{ anime.score }}</span>
-        <p>{{ anime.synopsis }}</p>
+        <div class="anime-links">
+            <NuxtLink :href="anime.url">See on My Anime List</NuxtLink>
+            <NuxtLink :href="anime.trailer.url">See the trailer</NuxtLink>
+        </div>
     </div>
 </template>
 
@@ -38,6 +53,62 @@ watchEffect(() => {
 
 </script>
 
-<style>
+<style scoped>
+.heading {
+    display: grid;
+    grid-template-columns: 35% 1fr;
+    justify-items: center;
+    align-items: center;
+}
 
+.div-img {
+    background-color: #213073;
+
+    width: 100%;
+    height: 100%;
+
+    text-align: center;
+}
+
+.div-img img {
+    border-radius: 7px;
+
+    height: 100%;
+}
+
+.titles {
+    text-align: center;
+}
+
+.production {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+
+.production div {
+    padding: 10px;
+}
+
+.anime-info {
+    display: flex; 
+    flex-direction: column;
+    justify-content: space-around;
+
+    padding: 10px 5px;
+}
+
+.anime-links {
+    display: flex;
+}
+
+.anime-links a {
+    padding: 10px 20px;
+    margin: 30px;
+
+    color: white;
+    background-color: #213073;
+
+    border-radius: 7px;
+}
 </style>
